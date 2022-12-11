939e604797224dd2f19e7344371d560bcbefb44f
Moongrail <omgone666@yandex.ru>
2022-12-11 16:50:40 +0300

    feat: add and change logic ManageServiceImpl

--- src/Main.java
+++ src/Main.java
@@ -13,34 +13,29 @@ public class Main {
         System.out.println("Поехали!");
 
         ManageService manageService = new ManageServiceImpl();
-        Task firstEpic = new Epic(1, "Купить продукты",
-                "Сходить в магазин за продуктами");
-        SubTask firstSubTask = new SubTask(3, "Купить помидоры", "Найти свежие помидоры в магазине."
-                , TaskStatus.NEW, firstEpic.getId());
-        SubTask secondSubTask = new SubTask(4, "Купить пиццу", "Заказать пиццу в кафе"
-                , TaskStatus.NEW, firstEpic.getId());
 
-        Task secondEpic = new Epic(2, "Спорт",
-                "Побегать в парке.");
-        SubTask thirdSubTask = new SubTask(5, "Обман", "Боже, иди домой, какой из тебя спортсмен.",
+        Epic firstEpic = new Epic(1,"Купить продукты", "Сходить в магазин за продуктами");
+        SubTask firstSubTask = new SubTask(3,"Купить помидоры", "Найти свежие помидоры в магазине."
+                , TaskStatus.NEW, firstEpic.getId());
+        SubTask secondSubTask = new SubTask(4,"Купить пиццу", "Заказать пиццу в кафе"
+                , TaskStatus.NEW, firstEpic.getId());
+        Epic secondEpic = new Epic(2,"Спорт", "Побегать в парке.");
+        SubTask thirdSubTask = new SubTask(5,"Обман", "Боже, иди домой, какой из тебя спортсмен.",
                 TaskStatus.DONE, secondEpic.getId());
+        Task simpleTask = new Task(77,"Просто задание", "Просто задание и никак " +
+                "не связано с эпиком или подзадачей эпика.", TaskStatus.DONE);
 
-        Task simpleTask = new Task(77, "Просто задание", "Просто задание и никак " +
-                "не связано с эпиком или подзадачей эпика.",
-                TaskStatus.DONE);
-
-        manageService.addTask(firstEpic);
-        manageService.addTask(secondEpic);
+        manageService.addEpic(firstEpic);
+        manageService.addEpic(secondEpic);
         manageService.addSubTask(firstEpic.getId(), firstSubTask);
         manageService.addSubTask(firstEpic.getId(), secondSubTask);
         manageService.addSubTask(secondEpic.getId(), thirdSubTask);
         manageService.addTask(simpleTask);
 
         //Распечатайте списки эпиков, задач и подзадач
-        HashMap<Integer, Task> allTaskList = manageService.getAllTaskList();
-        allTaskList.forEach((integer, task) -> System.out.println(integer + ": " + task));
+        HashMap<Integer, Task> allTaskMap = manageService.getAllMap();
+        allTaskMap.forEach((integer, task) -> System.out.println(integer + ": " + task));
         System.out.println("###########################################");
-
         /* Измените статусы созданных объектов, распечатайте.
          Проверьте, что статус задачи и подзадачи сохранился,
          а статус эпика рассчитался по статусам подзадач.
@@ -49,16 +44,16 @@ public class Main {
         SubTask updateSecondSubTask = new SubTask(4, "Испечь пиццу", "Самому приготовить пиццу.",
                 TaskStatus.DONE, firstEpic.getId());
 
-        manageService.updateTask(updateSecondSubTask);
+        manageService.updateSubTask(updateSecondSubTask);
         System.out.println(manageService.getTaskById(firstEpic.getId()));
         System.out.println("###########################################");
 
         //И, наконец, попробуйте удалить одну из задач и один из эпиков.
-        System.out.println("Удаление 1: " + manageService.removeTaskById(firstEpic.getId()));
-        System.out.println("Удаление 2: " + manageService.removeTaskById(thirdSubTask.getId()));
+        System.out.println("Удаление Эпика 1: " + manageService.removeEpicById(firstEpic.getId()));
+        System.out.println("Удаление Сабтаска 5: " + manageService.removeSubTask(thirdSubTask.getId()));
         System.out.println("###########################################");
-        HashMap<Integer, Task> finalAllTaskList = manageService.getAllTaskList();
-        finalAllTaskList.forEach((integer, task) -> System.out.println(integer + ": " + task));
+        HashMap<Integer, Task> finalAllTaskMap= manageService.getAllMap();
+        finalAllTaskMap.forEach((integer, task) -> System.out.println(integer + ": " + task));
 
     }
 }
